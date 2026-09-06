530715aab3505eb3ce264bfd8be9eff30dec7c45
MehdiKKoohneshin0 <mehdikoohneshin2002@gmail.com>
2026-09-06 23:42:22 +0330

    copy product url btn added

--- src/Components/Templates/Product/Content/HeroDetails/Fragments/UsefulButtons/CopyUrl.jsx
+++ src/Components/Templates/Product/Content/HeroDetails/Fragments/UsefulButtons/CopyUrl.jsx
@@ -1,10 +1,25 @@
 import { FaShare } from "react-icons/fa6";
 import Tooltip from "../../../../../../Ui/Tooltip";
+import { useState } from "react";
 
 const CopyUrl = () => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopyUrl = () => {
+    const url = location.origin + location.pathname;
+    navigator.clipboard.writeText(url);
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1500);
+  };
   return (
-    <Tooltip>
-      <button className="cursor-pointer size-8 text-sm hover:*:text-lg *:duration-150 *:transition-all active:*:text-sm flex-center border border-neutral-300 rounded-lg text-slate-700">
+    <Tooltip text={isCopied ? "لینک کپی شد" : "اشتراک گذاری"}>
+      <button
+        onClick={handleCopyUrl}
+        className="cursor-pointer size-8 text-sm hover:*:text-lg *:duration-150 *:transition-all active:*:text-sm flex-center border border-neutral-300 rounded-lg text-slate-700"
+      >
         <FaShare />
       </button>
     </Tooltip>
